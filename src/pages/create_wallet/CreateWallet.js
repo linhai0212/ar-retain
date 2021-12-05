2602ba4ad0265f420ef5c27e1e1d40fa563c237b
linhai <630086711@qq.com>
2021-12-05 16:32:03 +0800

    first commit

---
+++ src/pages/create_wallet/CreateWallet.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Arweave from 'arweave';
+import { useState } from "react"
+
+function App() {
+  // Or manually specify a host
+  const arweave = Arweave.init({
+    host: '127.0.0.1',
+    port: 1984,
+    protocol: 'http'
+  });
+
+  const [wallet_address, setWalletAddress] = useState('')
+  arweave.wallets.generate().then((key) => {
+    console.log(key);
+    arweave.wallets.jwkToAddress(key).then((address) => {
+      console.log(address)
+      setWalletAddress(address)
+
+      arweave.wallets.getBalance(address).then((balance) => {
+        let winston = balance;
+        let ar = arweave.ar.winstonToAr(balance);
+        console.log(winston);
+        console.log(ar);
+      });
+
+    })
+  });
+
+  return (
+    <div> **  {wallet_address} **</div>
+  );
+}
+
+export default App;
